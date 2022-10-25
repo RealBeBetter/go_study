9244058dcafae21bf92b8361665c6f03de350841
wei.song <wei.song@anker-in.com>
2022-10-25 14:45:13 +0800

    feat: add factory-pattern.go

--- object/main/factory-pattern.go
+++ object/main/factory-pattern.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
 	// 工厂模式
+	student := CreateStudent("Real", 22, 9)
+	fmt.Println(*student)
+	fmt.Println(student.GetStudentName())
+}
+
+type student struct {
+	Name  string
+	Age   int32
+	Grade int32
+}
+
+func CreateStudent(name string, age int32, grade int32) *student {
+	// 暴露函数名首字母大写的函数，重当构造方法
+	return &student{name, age, grade}
+}
+
+func (stu *student) GetStudentName() string {
+	return stu.Name
 }

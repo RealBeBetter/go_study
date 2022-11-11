3c01ad395583820ece96e9fda39f2a00b67d7919
Real <swrely@qq.com>
2022-11-12 01:03:45 +0800

    feat: add before_test.go

---
+++ testing/main/before_test.go
@@ -0,0 +1,16 @@
+/**
+ * @author: Real
+ * Date: 2022/11/12 0:55
+ */
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Testing start...")
+	// 执行测试
+	m.Run()
+}

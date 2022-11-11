4e70ac9ae81eff48cd9faabddd131783064ca04c
Real <swrely@qq.com>
2022-11-12 00:04:05 +0800

    feat: add deserialize_struct.go

--- serialize/main/deserialize_struct.go
+++ serialize/main/deserialize_struct.go
@@ -13,14 +13,27 @@ func main() {
 	// 反序列化 struct
 	var structSerialize = "{\"name\":\"Real\",\"age\":22}"
 	// 表示用该变量接收序列化的结果
-	var serializeType map[string]interface{}
+
+	/**
+	https://blog.csdn.net/qq_39697306/article/details/115085149
+	如果需要使用在同一个包中定义的其他文件中的结构体，需要将项目放到 $GOPATH/src 下
+	*/
+	// 同一个目录下面不能有个多 package main ，此处需要重新定义
+	var serializePerson PersonSerialization
 
 	// 调用反序列化方法
-	err := json.Unmarshal([]byte(structSerialize), &serializeType)
+	err := json.Unmarshal([]byte(structSerialize), &serializePerson)
 	if err != nil {
-		fmt.Println("json deserialize error , error = ", err)
+		fmt.Println("struct deserialize error , error = ", err)
 		return
 	}
 
-	fmt.Println(serializeType)
+	fmt.Println(serializePerson)
+}
+
+// PersonSerialization
+// 如果命名为大写，要想序列化字符串为小写，需要使用 json tag 名称
+type PersonSerialization struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }

df3aae8a26bb83fa84454f9b21f0b44ea106c66d
Real <swrely@qq.com>
2022-11-15 00:18:00 +0800

    feat: add basic_data_type.go

--- reflection/main/basic_data_type.go
+++ reflection/main/basic_data_type.go
@@ -4,7 +4,31 @@
  */
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 func main() {
 	// 基本数据类型进行反射
 
+	reflectBase(10)
+}
+
+func reflectBase(n int) {
+	typeOf := reflect.TypeOf(n)
+	fmt.Println("Type is", typeOf)
+	fmt.Println("The name of Type is", typeOf.Name())
+
+	valueOf := reflect.ValueOf(n)
+	fmt.Printf("Value is %d, the type of value is %T\n", valueOf, valueOf)
+
+	// 获取反射持有的整型值
+	i := valueOf.Int() + 3
+	fmt.Println("The value of i is", i)
+
+	inter := valueOf.Interface()
+	// 类型断言
+	num := inter.(int)
+	fmt.Println("num =", num)
 }

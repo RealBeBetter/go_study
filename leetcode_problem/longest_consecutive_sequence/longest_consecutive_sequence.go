75b09c710d923e6658d028f07fd152b84aeacff5
Real <swrely@qq.com>
2023-12-19 22:29:13 +0800

    feat: add problem—longest_consecutive_sequence

---
+++ leetcode_problem/longest_consecutive_sequence/longest_consecutive_sequence.go
@@ -0,0 +1,36 @@
+/**
+ * @author Real
+ * @since 2023/12/19 21:53
+ */
+package main
+
+//
+// longestConsecutive
+//  @Description: https://leetcode.cn/problems/longest-consecutive-sequence
+//  @param nums
+//  @return int
+//
+func longestConsecutive(nums []int) int {
+	numSet := make(map[int]bool)
+	for _, num := range nums {
+		numSet[num] = true
+	}
+
+	longestStreak := 0
+	for number := range numSet {
+		// 防止连续的序列需要重新计算
+		if !numSet[number-1] {
+			currentNumber := number
+			currentStreak := 1
+			for numSet[currentNumber+1] {
+				currentNumber++
+				currentStreak++
+			}
+			if currentStreak > longestStreak {
+				longestStreak = currentStreak
+			}
+		}
+	}
+
+	return longestStreak
+}

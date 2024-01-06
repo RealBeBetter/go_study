b5d3aa21650e6dee52080f77eff1d3cc7c17ec26
Real <swrely@qq.com>
2024-01-06 21:13:07 +0800

    feat: OAdd time complexity O(1) Lru cache

---
+++ data_structure/cache/lru_cache.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"container/list"
+)
+
+const defaultCapacity = 10
+
+type LRUCache struct {
+	KeyToNodeMap map[int]*list.Element
+	LinkedList   *list.List
+	Size         int
+	Capacity     int
+}
+
+type Node struct {
+	Key   int
+	Value int
+}
+
+func Constructor(capacity int) LRUCache {
+	lruCache := new(LRUCache)
+	lruCache.Size = 0
+	lruCache.Capacity = capacity
+	if capacity <= 0 {
+		lruCache.Capacity = defaultCapacity
+	}
+	lruCache.KeyToNodeMap = make(map[int]*list.Element)
+	lruCache.LinkedList = list.New()
+
+	return *lruCache
+}
+
+func (cache *LRUCache) Get(key int) int {
+	if valueNode, ok := cache.KeyToNodeMap[key]; ok {
+		// 移动到队尾
+		cache.LinkedList.MoveToBack(valueNode)
+		return valueNode.Value.(Node).Value
+	}
+
+	// 不存在则返回 -1
+	return -1
+}
+
+func (cache *LRUCache) Put(key int, value int) {
+	// 添加的时候存在值
+	if currentNode, ok := cache.KeyToNodeMap[key]; ok {
+		cache.LinkedList.Remove(currentNode)
+		// 将添加的节点放到队尾
+		cache.KeyToNodeMap[key] = cache.LinkedList.PushBack(Node{Key: key, Value: value})
+		return
+	}
+
+	// 不存在需要判断 size 是否符合要求
+	if cache.Capacity == cache.Size {
+		// 删除最久未使用的队头，map 中存储的 Key，所以需要自定义 Node
+		delete(cache.KeyToNodeMap, cache.LinkedList.Front().Value.(Node).Key)
+		cache.LinkedList.Remove(cache.LinkedList.Front())
+		cache.Size--
+	}
+
+	cache.LinkedList.PushBack(Node{Key: key, Value: value})
+	cache.KeyToNodeMap[key] = cache.LinkedList.Back()
+	cache.Size++
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
+
+func main() {
+	lruCache := Constructor(2)
+	lruCache.Put(1, 1)
+	lruCache.Put(2, 2)
+	println(lruCache.Get(1))
+	lruCache.Put(3, 3)
+	println(lruCache.Get(2))
+	lruCache.Put(4, 4)
+	println(lruCache.Get(1))
+	println(lruCache.Get(3))
+	println(lruCache.Get(4))
+}

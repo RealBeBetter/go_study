17893575cbc3fa35e4e253e1284c0a19d2a13bee
Real <swrely@qq.com>
2024-02-03 23:49:53 +0800

    feat[gee-module]: Add static file parsing and use

--- gee_framework/gee/gee.go
+++ gee_framework/gee/gee.go
@@ -2,6 +2,7 @@ package gee
 
 import (
 	"net/http"
+	"path"
 	"strings"
 )
 
@@ -56,4 +57,29 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := NewContext(w, req)
 	c.handlers = middlewares
 	engine.router.handle(c)
+}
+
+// create static handler
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+
+	return func(c *Context) {
+		file := c.GetParamVal("filepath")
+		// Check if file exists and/or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.StatusCode = http.StatusNotFound
+			return
+		}
+
+		fileServer.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Static serve static files
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	// Register GET handlers
+	group.GET(urlPattern, handler)
 }
